b39c1c41c60115b37b47dfda79659e5765beda3b
GustavoMacrini <gustavo.godencio@hotmail.com>
2023-08-15 19:13:58 -0300

    Projeto base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor (props){
+    super(props);
+    this.state = {
+      textoFrase: ''
+    };
+
+    this.frases = ['Siga os bons e aprenda com eles.', 
+                  'O bom-senso vale mais do que muito conhecimento.', 
+                  'O riso é a menor distância entre duas pessoas.', 
+                  'Deixe de lado as preocupações e seja feliz.',                  
+                  'Acredite em milagres, mas não dependa deles.',
+                  'A maior barreira para o sucesso é o medo do fracasso.'];
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);                  
+  }
+
+  quebraBiscoito(){
+    let state = this.state;
+    let random = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = '"' + this.frases[random] + '"';
+    this.setState(state);
+  }
+
+  render(){
+    return(
+      <section id='biscoito'>
+        <div className="container">
+          <img src={require('./assets/biscoito.png')} className='img'/>
+          <Botao nome='Abrir Biscoito' acaoBtn={this.quebraBiscoito}/>
+          <h3 className='frase'>{this.state.textoFrase}</h3>
+        </div>
+      </section>
+    );
+  }
+}
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <a onClick={this.props.acaoBtn} className='botao'>{this.props.nome}</a>
+      </div>
+    );
+  }
 }
 
 export default App;
